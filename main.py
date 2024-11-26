b1c6c02c1145d002b526ef783b1ef8677f2dd31e
asfour0406 <asfourbarmaja@gmail.com>
2024-11-26 01:52:42 -0600

    main file changes

--- main.py
+++ main.py
@@ -1,7 +1,24 @@
+import os
+import datetime
+#art
 copy =  open("./art/divider","r")
 divider = copy.read()
 copy =  open("./art/workout","r")
 workout = copy.read()
-print (workout)
-print (divider)
-print ("created by Ali Alasfour")
+copy =  open("./art/main","r")
+menu = copy.read()
+
+
+# date
+with open("./logs/lastOpened","r") as lastopened:
+    old = lastopened.read()
+    
+now = datetime.date.today()
+with open("./logs/lastOpened", "w") as f:
+    f.write(str(now))
+os.system('clear')
+menuInfo = (f"Last opened : {old}              by Ali Alasfour")
+#printing menu
+
+print(workout,"\n",divider,"\n",menuInfo,"\n",divider)
+print(menu)
--- test.py
+++
@@ -1,5 +0,0 @@
-import os
-os.system('clear')
-copy =  open("./art/inspo","r")
-inspo = copy.read()
-print(inspo)
